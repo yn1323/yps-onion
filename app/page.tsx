1ae26d728b3f7b9b9ba54172c904be42ecdeb9be
y-natani <yn1323@gmail.com>
2024-06-18 23:29:57 +0900

    supabase docker prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { createClient } from '@supabase/supabase-js';
 import './globals.css';
 
 export const metadata = {
   title: 'App Router',
 };
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const _supabase = createClient(supabaseUrl ?? '', supabaseKey ?? '');
 
 export default function Page() {
   return <h1 className="border text-gray-500">App Router</h1>;
